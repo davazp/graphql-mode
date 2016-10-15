7482c1b10077cc3df1e8be32727042b4cf8efb67
David Vazquez Pua <dvazquezpua@deloitte.nl>
2016-10-15 14:19:32 +0200

    Add basic commentary

--- graphql-mode.el
+++ graphql-mode.el
@@ -21,7 +21,18 @@
 
 ;;; Commentary:
 
-;; 
+;; This package implements a major mode to edit GraphQL schemas and
+;; query. The basic functionality includes:
+;;
+;;    - Syntax highlight
+;;    - Automatic indentation
+;;
+;; Additionally, it is able to
+;;    - Sending GraphQL queries to an end-point URL
+;;
+;; Files with the .graphql extension are automatically opened with
+;; this mode.
+
 
 ;;; Code:
 
