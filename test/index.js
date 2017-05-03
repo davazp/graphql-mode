eed3c6ca1c7b3ad77003985662f36d5a5ab3641f
David Vazquez Pua <dvazquezpua@deloitte.nl>
2017-05-03 14:44:35 +0200

    Create sample GraphQL server

---
+++ test/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var buildSchema = require('graphql').buildSchema;
+
+var fs = require('fs');
+
+var schemaContent = fs.readFileSync(__dirname + '/schema.graphql', 'utf-8');
+var schema = buildSchema(schemaContent);
+
+var root = { hello: () => 'Hello world!' };
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}));
+
+const PORT = 4000;
+
+app.listen(PORT, () => console.log(`Now browse to localhost:${PORT}/graphql`));
