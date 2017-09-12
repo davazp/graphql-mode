3965a4c37034d31a5ef24e88863a5b1adf34bb1c
Tim Shiu <punshiu@fb.com>
2017-09-12 09:41:30 -0700

    add request dependency

--- graphql-mode.el
+++ graphql-mode.el
@@ -4,7 +4,7 @@
 
 ;; Author: David Vazquez Pua <davazp@gmail.com>
 ;; Keywords: languages
-;; Package-Requires: ((emacs "24.3"))
+;; Package-Requires: ((emacs "24.3") (request "20170131.1747"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
